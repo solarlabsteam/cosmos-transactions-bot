aa78844ee9438e6762cf1d31742f747629a96408
Techno Freak <freak12techno@gmail.com>
2021-11-07 18:15:39 +0200

    feat: use cache for coingecko

--- coingecko.go
+++ coingecko.go
@@ -33,11 +33,23 @@ func (c *CoingeckoWrapper) GetRate() (float32, error) {
 		return 0, nil
 	}
 
+	if !c.lastUpdate.IsZero() && time.Since(c.lastUpdate).Minutes() > 10 {
+		log.Trace().
+			Time("now", time.Now()).
+			Time("then", c.lastUpdate).
+			Dur("diff", time.Since(c.lastUpdate)).
+			Msg("Using rate from cache.")
+		return c.result, nil
+	}
+
 	result, err := c.client.SimpleSinglePrice(c.currency, "USDT")
 	if err != nil {
 		log.Warn().Err(err).Msg("Could not get Coingecko exchange rate")
 		return 0, err
 	}
+
+	c.result = result.MarketPrice
+	c.lastUpdate = time.Now()
 
 	return result.MarketPrice, nil
 }
